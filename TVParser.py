1566e92498a7d2c3663d46471412e76796313bc5
Cory Krol <coryteee@gmail.com>
2018-05-19 13:50:06 -0500

    Added config file to speed up backtesting Fixed the problem of it not using fees in calculations. Estimates profit/loss with Kelly Leverage and Half-Kelly Leverage

--- TVParser.py
+++ TVParser.py
@@ -1,8 +1,20 @@
 import Trade
+import config_creator
 import math
 
-path = "trades.txt"
+path = config_creator.get_setting("./settings.ini", 'TradeInfo', 'trade-file-path')
+capital = float(config_creator.get_setting("./settings.ini", 'TradeInfo', 'initial-capital'))
 trades = []
+
+maker_fee = config_creator.get_setting("./settings.ini", 'TradeInfo', 'maker-fee')
+taker_fee = config_creator.get_setting("./settings.ini", 'TradeInfo', 'taker-fee')
+
+use_limit_orders = input("Use Limit Orders? [y/N]: ")
+use_limit_orders = str.upper(use_limit_orders)
+if use_limit_orders[0] == 'Y':
+    fee = float(maker_fee) / 100
+else:
+    fee = float(taker_fee) / 100
 
 with open(path, 'r') as f:
     # Throw away first 36 lines
@@ -42,7 +54,37 @@ with open(path, 'r') as f:
 
     kelly_leverage = mean/variance
 
-    print("\n\nMean Returns: " + str(100*mean) + '%')
-    print("Standard Deviation of Returns: " + str(100 * standard_deviation) + '%')
-    print("\n\nKelly Leverage: " + str(kelly_leverage))
-    print("Half Kelly: " + str(kelly_leverage/2))
+    print("\n\nMean Returns: " + '{0:.2f}'.format(100*mean) + '%')
+    print("Standard Deviation of Returns: " + '{0:.2f}'.format(100 * standard_deviation) + '%')
+    print("\n\nKelly Leverage: " + '{0:.2f}'.format(kelly_leverage))
+    print("Half Kelly: " + '{0:.2f}'.format(kelly_leverage/2))
+
+    trade_capital = capital
+    for i in trades:
+        if trade_capital < 0:
+            break
+        order_size = trade_capital * math.floor(kelly_leverage)
+        profit_loss = i.get_profit_loss() - (2 * fee)
+        capital_gain_loss = order_size * profit_loss
+        trade_capital = trade_capital + capital_gain_loss
+
+    profit_loss_net = trade_capital - capital
+    print("\n\nInitial Capital: $" + '{0:.2f}'.format(capital))
+    print("\n\nKelly Leverage: " + str(math.floor(kelly_leverage)))
+    print("Kelly Leveraged Profit/Loss: $" + '{0:.2f}'.format(profit_loss_net))
+    print("Kelly Leveraged P/L Percent: " + '{0:.2f}'.format(100*profit_loss_net/capital) + '%')
+
+    print("\n")
+    trade_capital = capital
+    for i in trades:
+        if trade_capital < 0:
+            break
+        order_size = trade_capital * math.floor(0.5 * kelly_leverage)
+        profit_loss = i.get_profit_loss() - (2 * fee)
+        capital_gain_loss = order_size * profit_loss
+        trade_capital = trade_capital + capital_gain_loss
+
+    profit_loss_net = trade_capital - capital
+    print("\n\nHalf Kelly: " + str(math.floor(kelly_leverage / 2)))
+    print("Half Kelly Leveraged Profit/Loss: $" + '{0:.2f}'.format(profit_loss_net))
+    print("Half Kelly Leveraged P/L Percent: " + '{0:.2f}'.format(100*profit_loss_net/capital) + '%')
---
+++ config_creator.py
@@ -0,0 +1,90 @@
+import configparser
+import os
+
+
+def create_config(path):
+    """
+    Create a config file
+
+    Ex Interpolation
+        vars={"font": "Arial", "font_size": "100"}
+    :param path:
+    :return:
+    """
+    config = configparser.ConfigParser()
+
+    config.add_section("TradeInfo")
+    config.set("TradeInfo", "trade-file-path", "trades.txt")
+    config.set("TradeInfo", "initial-capital", "500")
+    config.set("TradeInfo", "maker-fee", "0.16")
+    config.set("TradeInfo", "taker-fee", "0.20")
+
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
+def get_config(path):
+    """
+    Returns the config object
+    :param path:
+    :return:
+    """
+    if not os.path.exists(path):
+        create_config()
+
+    config = configparser.ConfigParser()
+    config.read(path)
+    return config
+
+
+def get_setting(path, section, setting):
+    """
+    Print out a setting
+    :param path:
+    :param section:
+    :param setting:
+    :return:
+    """
+    config = get_config(path)
+    value = config.get(section,setting)
+    msg = "{section} {setting} is {value}".format(
+        section=section, setting=setting, value=value
+    )
+
+    print(msg)
+    return value
+
+
+def update_setting(path, section, setting, value):
+    """
+    Update a setting
+    :param path:
+    :param section:
+    :param setting:
+    :param value:
+    :return:
+    """
+    config = get_config(path)
+    config.set(section, setting, value)
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
+def delete_setting(path, section, setting):
+    """
+    Delete a setting
+    :param path:
+    :param section:
+    :param setting:
+    :return:
+    """
+    config = get_config(path)
+    config.remove_option(section, setting)
+    with open(path, "w") as config_file:
+        config.write(config_file)
+
+
+if __name__ == "__main__":
+    path = "./settings.ini"
+    if not os.path.exists(path):
+        create_config(path)
